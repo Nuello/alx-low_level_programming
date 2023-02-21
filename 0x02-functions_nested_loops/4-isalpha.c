8ec088b8a3df001ad30c2c0d06f9477870bf61ec
Nuel lo <akalo.emmanuel18@gmail.com>
2023-02-21 14:47:14 +0100

    Created a program that checks if a variable is a letter

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _isalpha - checks for alphabetic character.
+ * @c: character to be checked if alphabetic.
+ *
+ * Return: 1 (Success)
+ * otherwise (0)
+ */
+
+int _isalpha(int c)
+{
+	if ((c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z' ))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
