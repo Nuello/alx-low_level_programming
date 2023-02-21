0739d03c86a021e29f9ebe087b41a3c8d5bf6f59
Nuel lo <akalo.emmanuel18@gmail.com>
2023-02-21 15:27:04 +0100

    A program that finds the absolute value of an integer

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _abs - computes the absol value of an int
+ *@n: The number to find the absolute value.
+ * Return: abs of n
+ */
+
+int _abs(int n)
+{
+	if (n < 0)
+	{
+		n *= -1;
+		return (n);
+	}
+	else
+	{
+		return (n);
+	}
+}
