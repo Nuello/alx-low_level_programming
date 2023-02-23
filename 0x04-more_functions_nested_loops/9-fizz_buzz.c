bc6e66572274d794e017f2f05168e849679c59de
Nuel lo <akalo.emmanuel18@gmail.com>
2023-02-23 20:02:11 +0100

    Created a FizzBuzz program

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * main - prints numbers from 1 to 100, print fizz for multiples of 3,
+ * buzz for multiples of 5 and fizzbuzz for multiples of 3 and 5
+ * Return: 0(Success)
+ */
+
+void main(void)
+{
+	int num = 1;
+
+	for (; num <= 100; num++)
+	{
+		if ((num % 5 == 0) && (num % 3 == 0))
+		{
+			printf("FizzBuzz ");
+		}
+		else if ((num % 3) == 0)
+		{
+			printf("Fizz ");
+		}
+		else if ((num % 5) == 0)
+		{
+			printf("Buzz ");
+		}
+		else
+		{
+		printf("%d ", num);
+		}
+	}
+}
