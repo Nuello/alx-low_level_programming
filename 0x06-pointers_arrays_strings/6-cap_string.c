19c6aa73b5b07bedf87db583b85f04a65ab41fbf
Nuel lo <akalo.emmanuel18@gmail.com>
2023-03-03 16:53:58 +0100

    A program that capitalizes all words of a string

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -8,7 +8,8 @@
 
 char *cap_string(char *str)
 {
-	char separ[] = {' ','\t','\n',',',';','.','!','?','"','(',')','{','}' };
+	char separ[] = {' ', '\t', '\n', ',', ';', '.', '!', '?', '"', '(',
+	')', '{', '}'};
 	int i, j;
 
 	for (i = 0; str[i] != 0; i++)
@@ -27,26 +28,3 @@ char *cap_string(char *str)
 	}
 	return (str);
 }
-
-/*char *cap_string(char *str)
-{
-	int i = 0;
-
-	while (str[i] != 0)
-	{
-		if( (str[i - 1] == ' ' || str[i-1] == '\t' || str[i-1] == '\n' 
-		|| str[i-1] == ',' || str[i-1] == ';' || str[i-1] == '.' 
-		|| str[i-1] == '!' || str[i-1] == '?' || str[i-1] == '"' 
-		|| str[i-1] == '(' || str[i-1] == ')' || str[i-1] == '{' 
-		|| str[i-1] == '}' || i == 0) && (str[i] >= 'a' && str[i] <= 'z'))
-		{
-			str[i] = str[i] - 32;
-		}
-		if (str[i - 1] == '\t')
-		{
-			str[i-1] = 32;
-		}
-		i++;
-	}
-	return (str);
-}*/
