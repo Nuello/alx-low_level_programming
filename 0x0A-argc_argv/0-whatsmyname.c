a446099150be88960a5f95057308d08d67b62e90
Nuel lo <akalo.emmanuel18@gmail.com>
2023-03-11 03:09:43 +0100

    Uploading 0-whatsmyname.c file

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - prints the name of program.
+ * @argc: the number of argument passed
+ * @argv: the vector that hold the argument passed.
+ * Return: Always 0 (success)
+ */
+
+int main(int argc, char *argv[])
+{
+	(void)argc;
+	printf("%s\n", argv[0]);
+	return (0);
+}
